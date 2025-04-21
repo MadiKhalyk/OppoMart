d1b4a5c3d139e48a27cc5f5b2f7a6599a36bef58
MadiKhalyk <madikhalyk07@gmail.com>
2025-04-21 20:19:59 +0500

    front

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Filament\Models\Contracts\FilamentUser;
 use Filament\Tables\Columns\Layout\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -55,9 +56,9 @@ class User extends Authenticatable implements FilamentUser
         return $this->belongsTo(WhatsappChat::class, 'id', 'user_id');
     }
 
-    public function purchase(): BelongsTo
+    public function purchase(): HasOne
     {
-        return $this->belongsTo(Purchase::class)->latest()->first();
+        return $this->hasOne(Purchase::class)->latestOfMany();
     }
 
 
