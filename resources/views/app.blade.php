a1cff3707095ad8028626710a2f699869f519462
MadiKhalyk <madikhalyk07@gmail.com>
2025-04-21 10:46:24 +0500

    front

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Магазин</title>
 
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+    @vite(['resources/js/app.js'])
 
     <link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.png') }}"/>
 
